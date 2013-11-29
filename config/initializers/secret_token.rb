04e019b6f19d8a610007cbf20f57f5fe16446677
Zack Siri <zack@artellectual.com>
2013-11-30 00:29:30 +0700

    initial init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kontax::Application.config.secret_key_base = '9e0ff970d9e0e665455c495128b4bdab55c503dfb7ffe80d44d1a2dc14f66293af861901c8624ec6cea3bcd43ce3559b8f7594ac7b3025bce32c5a33a7a74cd3'
