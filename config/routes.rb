7b42d81626ceab20310023e1bcf4535159ca30f9
Zack Siri <zack@artellectual.com>
2013-12-06 21:30:13 +0700

    fix contacts scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Kontax::Application.routes.draw do
   resources :contacts do 
-    get   'scope/:scope',   to: 'contacts#index',  as: :case,   on: :collection
+    get   'scope/:scope',   to: 'contacts#index',  as: :scope,   on: :collection
     match 'case/:case',     to: 'contacts#update', as: :case,   on: :member, via: [:put, :patch]
     get   'notify/:notify', to: 'contacts#show',   as: :notify, on: :member
   end 
