9800d13a6920c0b5c90ed8f0a579b0d2ae83ebb0
Zack Siri <zack@artellectual.com>
2013-12-06 15:17:06 +0700

    ran trash migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201083923) do
+ActiveRecord::Schema.define(version: 20131206081249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20131201083923) do
     t.string   "twitter"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "trash",      default: false
   end
 
 end
